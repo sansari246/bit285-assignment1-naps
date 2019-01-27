ce92c64ad45bf0f54b362b7d0d7e56ff06189241
Sansari <Sarah.ansari246@gmail.com>
2019-01-26 17:34:20 -0800

    a

---
+++ bit285-assignment1-naps/Controllers/NapsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace bit285assignment1naps.Controllers
+{
+    public class NapsController : Controller
+    {
+        // GET: /<controller>/
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //[HttpPost]
+        //public IActionResult Index()
+        //{
+        //    //Spin spin = new Spin();
+        //    //spin.Luck = player.Lucky;
+        //    //return RedirectToAction("SpinIt", spin);
+
+        //    return View();
+
+        //}
+    }
+}
--- bit285-assignment1-naps/Startup.cs
+++ bit285-assignment1-naps/Startup.cs
@@ -15,17 +15,28 @@ namespace bit285_assignment1_naps
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            app.UseFileServer();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("Default",
+                                "{controller=Naps}/{action=AccountInfo}/{id?}");
+            }
+        );
+
+            app.UseStaticFiles();
+
+        //  app.UseFileServer();
         }
     }
 }
